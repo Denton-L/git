92065a0d9535dcf00a9706fc1dfde517524abf6f
Denton Liu <liu.denton@gmail.com>
2019-10-30 03:31:35 -0700

    manage-patchset.sh: rename from create-patchset.sh

---
+++ manage-patchset.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+subcommand="$1"
+branch="$2"
+if test -z "$branch"
+then
+    branch="$(git branch --show-current)"
+    no_prefix="${branch##submitted/}"
+    if test "$branch" = "$no_prefix"
+    then
+	echo Missing \'submitted/\' prefix
+	exit 1
+    fi
+    branch="$no_prefix"
+fi
+
+patchdir="$(dirname "$0")"
+outdir="$patchdir/$branch"
+
+case "$subcommand" in
+create)
+	mkdir "$outdir"
+	git config --file="$outdir/config" format.outputDirectory "patches/$branch"
+	git config --file="$patchdir/common-config" includeIf."onbranch:submitted/$branch".path "$branch/config"
+	;;
+*)
+	echo Invalid subcomand: $subcommand
+	exit 1
+	;;
+esac
