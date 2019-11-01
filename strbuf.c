1f9e7415a417178544a26108ee5c835011fe44dd
Denton Liu <liu.denton@gmail.com>
2019-11-01 01:31:54 -0700

    strbuf: change confusing logic in strbuf_getline
    At first glance, it might seem like this function may have a buffer
    underflow bug. Without carefully reading, it might seem like it's
    possible where in the case where strbuf_getwholeline() reads nothing,
    `sb->len == 0`. In this case, the logic to strip the trailing newline
    would cause a buffer underflow as the computed offset would be negative.
    However, since this doesn't happen, this code doesn't actually have a
    buffer underflow. Even though there isn't a real bug, developers may
    waste their time pondering about whether or not this code is correct.
    
    In addition to this, the intent behind the nested ifs isn't immediately
    obvious.
    
    Replace the trailing newline stripping logic with
    strbuf_trim_trailing_newline() which is mostly the same but already
    handles the buffer underflow case for us since a little defensive
    programming never hurt anyone. Also, strbuf_trim_trailing_newline() has
    a self-documenting name and clearly explains the intent to any
    future readers.

--- strbuf.c
+++ strbuf.c
@@ -688,11 +688,7 @@ int strbuf_getline(struct strbuf *sb, FILE *fp)
 {
 	if (strbuf_getwholeline(sb, fp, '\n'))
 		return EOF;
-	if (sb->buf[sb->len - 1] == '\n') {
-		strbuf_setlen(sb, sb->len - 1);
-		if (sb->len && sb->buf[sb->len - 1] == '\r')
-			strbuf_setlen(sb, sb->len - 1);
-	}
+	strbuf_trim_trailing_newline(sb);
 	return 0;
 }
 
