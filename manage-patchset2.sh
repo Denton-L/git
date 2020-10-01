1df0eac914e82270f0c0c9c17fc588a530b9017d
Denton Liu <liu.denton@gmail.com>
2020-10-01 14:17:59 -0700

    manage-patchset2.sh: learn format-patch

--- manage-patchset2.sh
+++ manage-patchset2.sh
@@ -91,6 +91,55 @@ next)
 			git config --file="$patchdir/common-config" "branch.$next_branch.${key##*.}" $(git config "$key")
 		done
 	;;
+format-patch)
+	if test -z "$version"
+	then
+		die "no version number given"
+	fi
+
+	base=
+	reply_to=
+	while test $# != 0
+	do
+		case "$1" in
+			--in-reply-to)
+				reply_to="$2"
+				shift
+				;;
+			--*)
+				die "invalid option: $1"
+				;;
+			*)
+				break
+				;;
+		esac
+		shift
+	done
+
+	if test $# = 0
+	then
+		die "no base given"
+	fi
+	base="$1"
+	shift
+
+	reroll_count=
+	range_diff=
+	in_reply_to=
+
+	if test "$version" -gt 1
+	then
+		prev_version=$(($version - 1))
+		reroll_count="--reroll-count=$version"
+		range_diff="--range-diff=$base..$name/v$prev_version"
+	fi
+	if test -n "$reply_to"
+	then
+		in_reply_to=--in-reply-to="$reply_to"
+	fi
+
+	git -c "include.path=$PWD/$outdir/config" format-patch ${reroll_count:+"$reroll_count"} ${range_diff:+"$range_diff"} ${in_reply_to:+"$in_reply_to"} "$base"
+	;;
 *)
 	die "invalid subcomand: $subcommand"
 	;;
