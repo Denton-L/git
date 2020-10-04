a09680c23bcfeb1e23b900b86e4352192979ebc8
Denton Liu <liu.denton@gmail.com>
2020-10-04 02:28:34 -0700

    manage-patchset.sh: use the new and improved version

--- manage-patchset.sh
+++ manage-patchset.sh
@@ -1,64 +1,147 @@
 #!/bin/sh
 
-subcommand="$1"
-branch="$2"
+die () {
+	echo "$@"
+	exit 1
+}
 
-infer_branch=
+while test $# != 0
+do
+	case "$1" in
+	--branch)
+		branch="$2"
+		shift
+		;;
+	--*)
+		die "invalid option: $1"
+		;;
+	*)
+		break
+		;;
+	esac
+	shift
+done
+
+if test $# = 0
+then
+	die "no subcommand given"
+fi
+subcommand="$1"
+shift
+
+name=
+version=
 
 if test -z "$branch"
 then
 	branch="$(git branch --show-current)"
-	infer_branch=true
 fi
 
 case "$branch" in
-submitted/*)
-	;;
-*/)
-	if test -n "$infer_branch"
-	then
-		echo BUG: inferred branch with trailing slash
-		exit 1
-	fi
-	branch="submitted/${branch%/}"
+*/*)
+	name="${branch%/*}"
+	version="${branch##*/v}"
 	;;
 *)
-	if test -n "$infer_branch"
-	then
-		echo Missing \'submitted/\' prefix
-		exit 1
-	else
-		branch="submitted/$branch"
-	fi
+	name="$branch"
 	;;
 esac
-branch_part="${branch##submitted/}"
+
+case "$version" in
+*[!0-9]*)
+	die "bad version number: $version"
+	;;
+esac
 
 patchdir="$(dirname "$0")"
-outdir="$patchdir/$branch_part"
+outdir="$patchdir/$name"
 
 case "$subcommand" in
 create)
-	mkdir "$outdir"
-	git config --file="$outdir/config" format.outputDirectory "patches/$branch_part"
-	git config --file="$patchdir/common-config" includeIf."onbranch:$branch".path "$branch_part/config"
+	mkdir -p "$outdir"
+	git config --file="$outdir/config" format.outputDirectory "patches/$name"
+	git config --file="$patchdir/common-config" includeIf."onbranch:$name/*".path "$name/config"
 	;;
 remove)
 	rm -r "$outdir"
-	git config --file="$patchdir/common-config" --remove-section includeIf."onbranch:$branch"
-	git config --file="$patchdir/common-config" --remove-section branch."$branch"
+	git config --file="$patchdir/common-config" --remove-section includeIf."onbranch:$name/*".path
+	git config --get-regexp --name-only 'branch\.'"$name"'/.*' | while read key
+		do
+			git config --file="$patchdir/common-config" --unset "$key"
+		done
+	git for-each-ref --format='delete %(refname)' "refs/heads/$name/*" | git update-ref --stdin
 	;;
 sync)
-	git config --get-regexp --name-only branch\\."$branch"\\.\* |
-		while IFS='
-' read key
+	git config --get-regexp --name-only 'branch\.'"$name"'/.*' | while read key
 		do
 			git config --file="$patchdir/common-config" "$key" "$(git config "$key")"
+			git config --unset "$key"
 		done
-	git config --remove-section branch."$branch"
+	;;
+next)
+	if test -z "$version"
+	then
+		die "no version number given"
+	fi
+
+	next_version=$(($version + 1))
+	next_branch="$name/v$next_version"
+	git branch "$next_branch"
+	git config --get-regexp --name-only 'branch\.'"$name/v$version"'\..*' | while read key
+		do
+			git config --file="$patchdir/common-config" "branch.$next_branch.${key##*.}" $(git config "$key")
+		done
+	;;
+format-patch)
+	if test -z "$version"
+	then
+		die "no version number given"
+	fi
+
+	base=
+	reply_to=
+	while test $# != 0
+	do
+		case "$1" in
+			--in-reply-to)
+				reply_to="$2"
+				shift
+				;;
+			--*)
+				die "invalid option: $1"
+				;;
+			*)
+				break
+				;;
+		esac
+		shift
+	done
+
+	if test $# = 0
+	then
+		die "no base given"
+	fi
+	base="$1"
+	shift
+
+	reroll_count=
+	range_diff=
+	in_reply_to=
+
+	if test "$version" -gt 1
+	then
+		prev_version=$(($version - 1))
+		reroll_count="--reroll-count=$version"
+		range_diff="--range-diff=$base..$name/v$prev_version"
+	fi
+	if test -n "$reply_to"
+	then
+		in_reply_to=--in-reply-to="$reply_to"
+	fi
+
+	git -c "include.path=$PWD/$outdir/config" format-patch ${reroll_count:+"$reroll_count"} ${range_diff:+"$range_diff"} ${in_reply_to:+"$in_reply_to"} "$base"
 	;;
 *)
-	echo Invalid subcomand: $subcommand
-	exit 1
+	die "invalid subcomand: $subcommand"
 	;;
 esac
